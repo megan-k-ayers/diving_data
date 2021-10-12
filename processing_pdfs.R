f43c88bea8787ac3dbb971d109f94cf2746b06a3
mka <m.ayers@yale.edu>
2021-10-11 20:30:05 -0400

    Cleaned up main run function of processing_pdfs.R and tested with a few more diving files, saving their results to csv in new folders now

--- processing_pdfs.R
+++ processing_pdfs.R
@@ -2,52 +2,91 @@ rm(list = ls())
 library(pdftools)
 source("./utils.R")
 
-# User provided inputs
-# pdf_path <- "./data/FINA_past/2018_FINA_World_Series_Montreal.pdf"
-# events <- c(" Men's 3m Springboard", "Women's 3m Springboard",
-#             " Men's 10m Platform", "Women's 10m Platform")
-# rounds <- c("Semifinal A", "Semifinal B", "Final")
+# ----------------------------------- SETUP -----------------------------------
 
-# pdf_path <- "./data/FINA_past/2015_FINA_World_Series_Dubai.pdf"
-# events <- c(" Men's 3m Springboard", "Women's 3m Springboard",
-#             " Men's 10m Platform", "Women's 10m Platform")
-# rounds <- c("Semifinal A", "Semifinal B", "Final")
-
-pdf_path <- "./data/omega_complete_results_book_2021_diving_world_cup.pdf"
-events <- c(" Men's 3m Springboard", "Women's 3m Springboard",
-            " Men's 10m Platform", "Women's 10m Platform")
-rounds <- c("Preliminary", "Semifinal", "Final")
-
-
-# Get the relevant page numbers for these events/rounds
-pages <- collect_pgs(events, rounds, pdf_path)
-
-# Concatenate results and judges into one big table, adding columns for event
-# and round
-results <- data.frame()
-judges <- data.frame()
-for (i in 1:length(pages)) {
+# Main function to collect all detailed results and judging data from one 
+# diving PDF. Utilizes functions in utils.R, and writes CSV files to `results`
+# and `judges`.
+run <- function(pdf_path, events, rounds) {
   
-  pages_name <- names(pages)[i]
-  ids <- unlist(strsplit(pages_name, "\\."))
-  ids <- gsub("_", " ", ids)
-  names(ids) <- c("round", "event", "type")
+  print(paste("Tabulating results and judges for", pdf_path))
   
-  print(paste("Tabulating", paste(ids, collapse = " ")))
+  # Get the relevant page numbers for these events/rounds
+  pages <- collect_pgs(events, rounds, pdf_path)
   
-  x <- get_pdf_data(pdf_path, pages[[i]])
-  
-  if (ids["type"] == "results") {
-    x <- tabulate_results(x, ids["event"], ids["round"])
-    results <- rbind(results, x)
+  # Concatenate results and judges into one big table, adding columns for event
+  # and round
+  results <- data.frame()
+  judges <- data.frame()
+  for (i in 1:length(pages)) {
+    
+    pages_name <- names(pages)[i]
+    ids <- unlist(strsplit(pages_name, "\\."))
+    ids <- gsub("_", " ", ids)
+    names(ids) <- c("round", "event", "type")
+    
+    x <- get_pdf_data(pdf_path, pages[[i]])
+    
+    if (ids["type"] == "results") {
+      x <- tabulate_results(x, ids["event"], ids["round"])
+      results <- rbind(results, x)
+    }
+    
+    if (ids[3] == "judges") {
+      x <- tabulate_judges(x, ids["event"], ids["round"])
+      judges <- rbind(judges, x)
+    }
+    
   }
   
-  if (ids[3] == "judges") {
-    x <- tabulate_judges(x, ids["event"], ids["round"])
-    judges <- rbind(judges, x)
-  }
+  # Write results and judges csv's
+  print(paste("Saving results and judges df's for", pdf_path))
+  
+  comp_name <- gsub("./data/FINA_past/|.pdf|FINA_", "", pdf_path)
+  write.csv(results,
+            paste("./detailed_results/", comp_name, "_RESULTS.csv", sep = ""),
+            row.names = FALSE)
+  write.csv(judges,
+            paste("./judges/", comp_name, "_JUDGES.csv", sep = ""),
+            row.names = FALSE)
   
 }
+
+
+# ------------------------------ GENERATING FILES -----------------------------
+
+# Names of rounds when we have prelims
+rounds_p <- c("Preliminary", "Semifinal", "Final")
+# Names of rounds when we have Semis A and B
+rounds_sf <- c("Semifinal A", "Semifinal B", "Final")
+
+# Events to grab
+events <- c(" Men's 3m Springboard", "Women's 3m Springboard",
+            " Men's 10m Platform", "Women's 10m Platform")
+
+
+# TOKYO WORLD CUP
+run(pdf_path = "./data/FINA_past/2021_FINA_World_Cup.pdf",
+    events = events, rounds = rounds_p)
+
+# MONTREAL 2018
+run(pdf_path = "./data/FINA_past/2018_FINA_World_Series_Montreal.pdf",
+    events = events, rounds = rounds_sf)
+
+# DUBAI 2015
+run(pdf_path = "./data/FINA_past/2015_FINA_World_Series_Dubai.pdf",
+    events = events, rounds = rounds_sf)
+
+# BEIJING 2017
+run(pdf_path = "./data/FINA_past/2017_FINA_World_Series_Beijing.pdf",
+    events = events, rounds = rounds_sf)
+
+# FUJI 2018
+run(pdf_path = "./data/FINA_past/2018_FINA_World_Series_Fuji.pdf",
+    events = events, rounds = rounds_sf)
+
+
+
 
 
 
